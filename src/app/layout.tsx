36997ebee052193d9fa761ebc9b85da9a4fdcd0e
raminka13 <raminka13@hotmail.com>
2026-07-07 04:19:05 -0500

    feat: add loading skeleton component for dynamic page loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.className} antialiased`}>
         <ThemeProvider>
           <Navbar />
 
